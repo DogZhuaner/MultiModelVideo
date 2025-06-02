e153ff0dba190b5ab09e3f434ad634fff349ffee
DogZhuaner <dogzhuaner@gmail.com>
2025-06-02 17:43:04 +0800

    update

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 import time
 from cameraManager import camera
 from capture_logic import check_and_capture_once
+def start_handDetecton():
+    try:
+        while True:
+            check_and_capture_once()
+            time.sleep(0.05)  # 避免过度占用CPU
+    except KeyboardInterrupt:
+        print("退出")
+    finally:
+        camera.release()
 
-try:
-    while True:
-        check_and_capture_once()
-        time.sleep(0.05)  # 避免过度占用CPU
-except KeyboardInterrupt:
-    print("退出")
-finally:
-    camera.release()
+if __name__ == '__main__':
+    start_handDetecton()
